e6b161ff23d306929eb68e641acbcb24551ad759
Александр <sanya.rogov37@gmail.com>
2017-11-29 23:58:51 +0500

    переделал

--- async.js
+++ async.js
@@ -1,56 +1,49 @@
 'use strict';
 
-exports.isStar = false;
+exports.isStar = true;
 exports.runParallel = runParallel;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
+ * @param {Number} timeout
  * @returns {Promise}
  */
-function runParallel(jobs, parallelNum) {
-    let i = parallelNum;
-    let dividedArray = [];
-    let resArray = [];
-    while (i <= jobs.length) {
-        dividedArray.push(jobs.slice(i - parallelNum, i));
-        i += parallelNum;
-    }
-    let resPromise = Promise.resolve([]);
-    dividedArray.forEach(parallelActions => {
-        resPromise = resPromise.then(function (prevData) {
-            if (prevData.length) {
-                resArray.push(prevData);
-            }
+function runParallel(jobs, parallelNum, timeout = 1000) {
+    let actions = [...jobs];
+    let result = [];
+    let nextIndex = parallelNum;
+    let startJobs = actions.splice(0, parallelNum);
 
-            return Promise.all(parallelActions.map(catchError))
-                .catch(function (error) {
-                    resArray.push(error);
-
-                    return error;
-                });
+    return new Promise(function (resolve) {
+        startJobs.forEach(function (job, i) {
+            makePromise(preparePromise(job), i, resolve);
         });
     });
 
-    return resPromise.then(function (prevData) {
-        resArray.push(prevData);
-
-        return Promise.resolve([].concat(...resArray));
-    });
-    function makePromise(action) {
+    function preparePromise(job) {
         return new Promise(function (res, rej) {
-            try {
-                let result = action();
-                res(result);
-            } catch (e) {
-                rej(e);
-            }
+            job().then(res)
+                .catch(rej);
+            setTimeout(function () {
+                rej(new Error('Promise timeout'));
+            }, timeout);
         });
     }
-    function catchError(action) {
-        return Promise.resolve(makePromise(action)).catch(function (error) {
-            return error;
-        });
+    function makePromise(promise, index, initCallback) {
+        promise.then(writePromiseResult.bind(null, index, initCallback))
+            .catch(writePromiseResult.bind(null, index, initCallback));
+    }
+    function writePromiseResult(index, initCallback, data) {
+        result[index] = data;
+        if (!actions.length) {
+            setTimeout(function () {
+                initCallback(result);
+            }, timeout);
+
+            return;
+        }
+        makePromise(preparePromise(actions.shift()), nextIndex++, initCallback);
     }
 }
 
